094eefba74ba28a42445ed75a41ecb7b5dfe9117
tara-hss <tara-hsseini@gmail.com>
2024-01-26 12:38:15 +1100

    Fixes the quote typos

--- src/components/QuoteSection.js
+++ src/components/QuoteSection.js
@@ -5,7 +5,7 @@ export default function QuoteSection() {
     return (
         <div className="section quote">
             <p className="quote-text"><FontAwesomeIcon icon={faQuoteLeft} />
-                Food is everything we are. It's an extention of nationalist feeling. ethnic feeling. Your personal history, your province, your rigion, your tribe, your grandma. It's inseparable from those from the get-go.
+                Food is everything we are. It's an extension of nationalist feeling,ethnic feeling. Your personal history, your province, your region, your tribe, your grandma. It's inseparable from those from the get-go.
             </p>
             <p className="quote-author">
                 - Anthony Bourdain
